eecb09e164001f52f06471ad8390da0975539400
cristian <styven21121@gmail.com>
2020-04-24 18:59:25 -0500

    fix(database-provider): ajuste en develop

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -2,14 +2,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
 import { ConnectionOptions } from 'tls';
 import { Configuration } from '../config/config.keys';
+import { Logger } from '@nestjs/common';
 
 export const databaseProvider = [
   MongooseModule.forRootAsync({
     imports: [ConfigService],
     inject: [ConfigService],
     async useFactory(config: ConfigService) {
+      Logger.debug(config.get(Configuration.DB_URI), 'Db Uri');
       return {
-        ssl: true,
         uri: config.get(Configuration.DB_URI),
         useFindAndModify: false,
         useNewUrlParser: true,
