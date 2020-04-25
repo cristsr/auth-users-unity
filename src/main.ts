d2aa2c30d17351499d1ab4903713405a81d041ff
cristian <styven21121@gmail.com>
2020-04-24 19:11:50 -0500

    fix(mongo-uri): se quemo la uri en database.provider

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(AppModule.port);
+  await app.listen(3000);
   Logger.log('Application runing at port ' + AppModule.port, 'Bootstrap')
 }
 bootstrap();
